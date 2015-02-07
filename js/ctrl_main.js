ea6658fcbcc9ee1f5ac269ac3ad7e039650da047
xuxiaoyu89 <xuxiaoyu89@gmail.com>
2015-02-07 06:02:42 -0500

    Update ctrl_main.js

--- js/ctrl_main.js
+++ js/ctrl_main.js
@@ -55,7 +55,7 @@ geoHunterControllers.controller('MainCtrl', ['$scope', '$log', '$http',
                     
                 }
                 else{
-                    "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY"
+                    //  "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY"
                     var key = "AIzaSyCdEJQfa3i2ul0Ji3p4xFuc9DNA1KY4HNw";
                     var address = $scope.address;
                     for (i=0; i<address.length; i++){
