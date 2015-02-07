1a2d8b2a178a658f3cf2b13be88bd07c77f60494
Vangie Shue <vangieshue@gmail.com>
2015-02-07 02:08:40 -0500

    Update ctrl_play.js

--- js/ctrl_play.js
+++ js/ctrl_play.js
@@ -14,5 +14,19 @@ geoHunterControllers.controller('PlayCtrl',
     }
     getLocalVars();
     
+    function detectBadRedirect() {
+      var baddata = false;
+      if (longitude === null || latitude === null) {
+        baddata = true;
+      }
+      if (place_url === null) {
+        baddata = true;
+      }
+      if (baddata) {
+        $window.alert("You seem to have gotten here incorrectly...");
+        $window.location.href = "http://xuxiaoyu89.github.io/DevFest/";
+      }
+    }
+    
     
 });
