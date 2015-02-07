dfc728084a64eee3cc3f090e57044f47995db05d
Vangie Shue <vangieshue@gmail.com>
2015-02-07 01:42:53 -0500

    Update ctrl_play.js

--- js/ctrl_play.js
+++ js/ctrl_play.js
@@ -1,6 +1,18 @@
 geoHunterControllers.controller('PlayCtrl', ['$scope', '$document', '$routeParams', '$log',
   function($scope, $document, $routeParams, $log) {
     
+    //Define all non-local variables here
+    var longitude, latitude, place_url;
+    
+    function getLocalVars() {
+      longitude = $window.localStorage.getItem("lng");
+      latitude = $window.localStorage.getItem("lat");
+      place_url = $window.localStorage.getItem("place_url");
+      $log.info(longitude);
+      $log.info(latitude);
+      $log.info(place_url);
+    }
+    getLocalVars();
     
     
   }
