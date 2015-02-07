afe4203b265bfff22986491a623922f1edd4c886
Vangie Shue <vangieshue@gmail.com>
2015-02-07 07:54:11 -0500

    Update ctrl_main.js

--- js/ctrl_main.js
+++ js/ctrl_main.js
@@ -1,7 +1,7 @@
 geoHunterControllers.controller('MainCtrl', ['$scope', '$log', '$http', '$location',
     function($scope, $log, $http, $location) {
-        $scope.long = -73.9615376;
-        $scope.lat = 40.80942609;
+        $scope.long = -73.962; //5376;
+        $scope.lat = 40.809; //42609;
         $scope.address = "";
         var map;
         function initialize() {
