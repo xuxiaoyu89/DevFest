a1c057e159a27141c553ea881265d0dc6654b109
hezzze <hezzze@gmail.com>
2015-02-07 03:07:13 -0500

    updated ctrl_options.js and options.html with API calls and html skeleton

--- js/ctrl_options.js
+++ js/ctrl_options.js
@@ -1,7 +1,7 @@
 'use strict';
 
-geoHunterControllers.controller('OptionsCtrl', ['$scope', '$document', '$routeParams', '$log',
-    function($scope, $document, $routeParams, $log) {
+geoHunterControllers.controller('OptionsCtrl', ['$scope', '$document', '$routeParams', '$log', '$window', '$location',
+    function($scope, $document, $routeParams, $log, $window, $location) {
         var map;
         var service;
         var infowindow;
@@ -21,6 +21,20 @@ geoHunterControllers.controller('OptionsCtrl', ['$scope', '$document', '$routePa
             types: ['store']
         };
 
+        $scope.getUrl = function(obj) {
+            return "https://maps.googleapis.com/maps/api/streetview?size=300x300&location=" + obj.vicinity + "&fov=90&heading=235&pitch=10&key=AIzaSyDatZNTLLkiqVqtJ9gYulPoaN6SFLpdFYQ";
+        };
+
+        $scope.choosePic = function(obj) {
+            var loc = obj.geometry.location;
+
+            $window.localStorage.setItem("lng", loc.D);
+            $window.localStorage.setItem("lat", loc.k);
+            $window.localStorage.setItem("place_url", JSON.stringify($scope.getUrl(obj)));
+
+            $location.path('/play');
+        }
+
         service = new google.maps.places.PlacesService(map);
         service.nearbySearch(request, function(results, status) {
             if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -29,13 +43,18 @@ geoHunterControllers.controller('OptionsCtrl', ['$scope', '$document', '$routePa
                 //   createMarker(results[i]);
                 // }
 
-                console.log("A!")
+                console.log(results);
+
+                console.log(results[1].photos[0].getUrl({
+                    maxWidth: 35,
+                    maxHeight: 35
+                }));
 
                 $scope.results = results;
+
+
                 $scope.$digest();
             }
         });
     }
 ]);
-
-
